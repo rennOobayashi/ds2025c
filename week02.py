83789308f533b37fc813bd4f66905d9ad692e10a
inhee <devforplayer@gmail.com>
2025-03-17 15:21:28 +0900

    v0.3 random number game

--- week02.py
+++ week02.py
@@ -1,5 +1,22 @@
-n = int(input("enter integer number: "))
+import time
+import random
 
-result = n * (n + 1) // 2 # O(1)
+random.seed(time.time())
 
-print(result)
+random_a = random.randint(1, 100) + 1
+
+print("Random Integer")
+
+for i in range(7, 0, -1):
+    inp = int(input(f"Enter Answer(remain: {i}): "))
+
+    if inp > random_a :
+        print("down")
+    elif inp < random_a:
+        print("up")
+    else:
+        print("You win!")
+        break
+
+    if i <= 1:
+        print("You lose!")
