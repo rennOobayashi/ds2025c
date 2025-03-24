0c6b853fb170f63af2ff97169c3a78b9432a8552
inhee <devforplayer@gmail.com>
2025-03-24 15:06:52 +0900

    v4.5 find list duplicate

--- week03.py
+++ week03.py
@@ -1,5 +1,26 @@
-cities = ["Suwan", "Hwasung", "Incheon", "Incheon", "Bucheon", "Incheon", "Seoul"]
+def duplicate_city(ls):
+    result_city = set()
 
-cities = set(cities)
+    s = set()
+
+    for city in ls:
+        l1 = len(s)
+
+        s.add(city)
+
+        l2 = len(s)
+
+        if l1 == l2:
+            result_city.add(city)
+
+    return list(result_city)
+
+cities = ["Suwan", "Hwasung", "Incheon", "Incheon", "Bucheon", "Incheon", "Seoul"]
+cities.append("Seoul")
+cities.append("Anyang")
+cities.append("Incheon")
+
+dups = duplicate_city(cities)
 
 print(cities)
+print(dups)
