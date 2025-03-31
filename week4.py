bda82a7284bc9bfa5779136cfe3bf47c84799ca4
inhee <devforplayer@gmail.com>
2025-03-31 14:22:13 +0900

    v5.0 Linked List

---
+++ week4.py
@@ -0,0 +1,30 @@
+from typing import dataclass_transform
+
+
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head :
+            self.head = Node(data)
+            return
+
+        current = self.head
+
+        while current.link:
+            current = current.link #move current
+
+        current.link = Node(data)
+
+ls = LinkedList()
+ls.append(1)
+ls.append(2)
+ls.append(3)
+
+print(ls)
