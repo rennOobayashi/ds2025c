974cf80e4c20abe123732deebc4c2f0912be5d33
inhee <devforplayer@gmail.com>
2025-04-07 15:11:09 +0900

    v6.5 reverse string in python

--- week05.py
+++ week05.py
@@ -1,4 +1,21 @@
-s = "Nayutan Seijin"
+def check_parentheses(check_string : str) -> bool:
+    stack = []
 
-print(s[::-1])
-print(''.join(reversed(s)))
+    for data in check_string:
+        if data == '(':
+            stack.append(data)
+        elif data == ')':
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+
+    return len(stack) == 0
+
+s1 = "(Nayutan seinjin)"
+s2 = ")(Nayutan seinjin)("
+
+if check_parentheses(s1):
+    print("Normal.")
+else:
+    print("Wrong use of parentheses.")
