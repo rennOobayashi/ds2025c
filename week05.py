8acf61d18466a268a1739eea788ad60aa94572c1
inhee <devforplayer@gmail.com>
2025-04-07 14:57:11 +0900

    v6.4 reverse string in python

--- week05.py
+++ week05.py
@@ -1,16 +1,4 @@
-s1 = []
+s = "Nayutan Seijin"
 
-print(len(s1))
-s1.append("NayutanSeijin")
-s1.append("Deco27")
-print(len(s1))
-print(s1[-1])
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
-
-# index error
-# print(s1.pop())
-# print(s1)
+print(s[::-1])
+print(''.join(reversed(s)))
