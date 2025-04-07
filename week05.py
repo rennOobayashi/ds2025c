96378642cf15c69d6b5903b47c5c6c8aa35c1c3c
inhee <devforplayer@gmail.com>
2025-04-07 15:40:32 +0900

    v6.6 check parenttheses with hint

--- week05.py
+++ week05.py
@@ -1,23 +1,34 @@
 def check_parentheses(check_string : str) -> str:
-    stack = []
+    stack = dict()
+    stack["("] = []
+    stack["{"] = []
+    stack["["] = []
 
     for data in check_string:
-        if data == '(':
-            stack.append(data)
-        elif data == ')':
-            if len(stack) == 0:
+        if data == '(' or data == '{' or data == '[':
+            stack[data].append(data)
+        elif data == ')'  or data == '}' or data == ']':
+            d = ''
+            if data == ')': d = '('
+            elif data == '}': d = '{'
+            elif data == ']': d = '['
+
+            if len(stack[d]) == 0:
                 return ("Wrong use of parentheses\r\n"
                         "(The closing parentheses must apper after the opening parentheses)")
             else:
-                stack.pop()
-    if len(stack) == 0:
+                stack[d].pop()
+
+    if (len(stack["("]) + len(stack["{"]) + len(stack["["])) == 0:
         return "Normal"
     else:
         return ("Wrong use of parentheses\r\n"
                 "(Need closing parentheses)")
 
-s1 = "(Nayutan seijin)"
-s2 = ")(Nayutan seijin)("
+s1 = "[{(Nayutan seijin)}]"
+s2 = "}[(Nayutan seijin)[}"
+s3 = "[({Nayutan seijin)]}"
 
 print(check_parentheses(s1))
 print(check_parentheses(s2))
+print(check_parentheses(s3))
