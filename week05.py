2c6708e76e24934906a6d76614719e6d0dae7169
inhee <devforplayer@gmail.com>
2025-04-07 14:54:01 +0900

    v6.3 python default Stack

--- week05.py
+++ week05.py
@@ -1,58 +1,16 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.link = None
+s1 = []
 
-class Stack:
-    def __init__(self):
-        self.top = None
-        self.index_cnt = 0
-
-    def push(self, data):
-        node = Node(data)
-        self.index_cnt += 1
-
-        if self.top is None:
-            self.top = node
-        else:
-            node.link = self.top
-            self.top = node
-
-    def pop(self):
-        if self.top is None:
-            return print("This stack is empty")
-
-        self.index_cnt -= 1
-
-        popped_node = self.top
-        self.top = self.top.link
-        popped_node.link = None
-        return popped_node.data
-
-    def size(self):
-        return self.index_cnt
-
-    def is_empty(self):
-        return self.top is None
-
-    def peek(self):
-        return self.top.data
-
-    def print_stack(self):
-        data = self.top
-        while data is not None:
-            print(f"{data.data}", end=" ")
-            data = data.link
-
-        print()
-
-s = Stack()
-print(s.is_empty())
-s.push(1)
-print(s.is_empty())
-s.push(2)
-s.push(3)
-s.print_stack()
-# print(s.peek())
-# print(s.pop())
-# print(s.pop())
+print(len(s1))
+s1.append("NayutanSeijin")
+s1.append("Deco27")
+print(len(s1))
+print(s1[-1])
+print(s1)
+print(s1.pop())
+print(s1)
+print(s1.pop())
+print(s1)
+g
+# index error
+# print(s1.pop())
+# print(s1)
