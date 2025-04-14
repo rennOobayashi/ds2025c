54c986683dbfd7a49510a73d0f734b58a6588d19
inhee <devforplayer@gmail.com>
2025-04-14 15:06:56 +0900

    v7.3 Queue by pyhton lib

--- week06.py
+++ week06.py
@@ -1,60 +1,16 @@
-class Node:
-    def __init__(self, data, link=None):
-        self.data = data
-        self.link = link
-
-class Queue:
-    def __init__(self):
-        self.front = None
-        self.rear  = None
-        self.size = 0
-
-    def enqueue(self, data):
-        self.size += 1
-        node = Node(data)
-
-        if self.rear is None:
-            self.front = node
-            self.rear = node
-        else:
-            self.rear.link = node
-            self.rear = node
-
-    def dequeue(self):
-        if self.front is None:
-            raise IndexError("Queue is empty!")
-
-        self.size -= 1
-        temp = self.front
-        self.front = self.front.link
-
-        if self.front is None:
-            self.rear = None
-
-        temp.link = None
-
-        print(f"dequeue - {temp.data}")
-
-        return temp.data
-
-    def print_queue(self):
-        if self.front is None or self.rear is None:
-            print(self.size, self.front, self.rear)
-        else:
-            print(self.size, self.front.data, self.rear.data)
+from queue import Queue
 
 q = Queue()
-q.enqueue("NayutanSeijin")
-q.enqueue("Deco27")
-q.enqueue("PinocchioP")
 
-q.dequeue()
-# front = node, rear = node
-q.print_queue()
+q.put("NayutanSeijin")
+q.put("Deco27")
+q.put("PinocchioP")
 
-q.dequeue()
-q.print_queue()
+print(q.qsize())
+print(q.get())
 
-q.dequeue()
-# front = None, rear = None
-q.print_queue()
+print(q.qsize())
+print(q.get())
+
+print(q.qsize())
+print(q.get())
