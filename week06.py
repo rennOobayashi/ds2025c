eef06ad3d0667b3405192ceaf96bdaef69225a58
inhee <devforplayer@gmail.com>
2025-04-14 14:12:11 +0900

    v7.0 queue, enqueue

---
+++ week06.py
@@ -0,0 +1,28 @@
+class Node:
+    def __init__(self, data, link=None):
+        self.data = data
+        self.link = link
+
+class Queue:
+    def __init__(self):
+        self.front = None
+        self.rear  = None
+        self.size = 0
+
+    def enqueue(self, data):
+        self.size += 1
+        node = Node(data)
+
+        if self.rear is None:
+            self.front = node
+            self.rear = node
+        else:
+            self.rear.link = node
+            self.rear = node
+
+q = Queue()
+q.enqueue("NayutanSeijin")
+q.enqueue("Deco27")
+q.enqueue("PinocchioP")
+
+print(q.size, q.front.data, q.rear.data)
