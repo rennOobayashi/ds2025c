f4a3cc52af68a06ddf7649058c4895a32ec6ad21
inhee <devforplayer@gmail.com>
2025-04-28 15:43:37 +0900

    v8.3 Binary search tree method

--- week08.py
+++ week08.py
@@ -5,6 +5,49 @@ class TreeNode:
         self.right = None
         self.data = None
 
+    def insert(self, root, value):
+        node = TreeNode()
+        node.data = value
+
+        if root is None:
+            return node
+
+        current = root
+
+        while True:
+            if current.data > node.data:
+                if current.left is None:
+                    current.left = node
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right is None:
+                    current.right = node
+                    break
+                else:
+                    current = current.right
+
+        return root
+
+    def search(self, root, find_value):
+        c = root
+
+        while True:
+            if c is None:
+                print(f"cannot find {find_number}")
+                break
+            elif c.data is find_number:
+                print(f"find {find_number}!")
+                break
+            elif c.data > find_number:
+                c = c.left
+                continue
+            elif c.data < find_number:
+                c = c.right
+                continue
+
+
 def in_order(model):
     if model is None:
         return
@@ -14,50 +57,18 @@ def in_order(model):
     in_order(model.right)
 
 if __name__ == '__main__':
-    numbers = [10, 15, 8, 4, 9]
+    numbers = [10, 15, 8, 4, 9, 1, 7, 100, 5]
     root = None
 
     node = TreeNode()
-    node.data = numbers[0]
-    root = node
 
-    for i in numbers[1:]:
-        n = TreeNode()
-        n.data = i
+    for i in numbers:
+        root = node.insert(root, i)
 
-        current = root
-
-        while True:
-            if current.data > n.data:
-                if current.left is None:
-                    current.left = n
-                    break
-                else:
-                    current = current.left
-            else:
-                if current.right is None:
-                    current.right = n
-                    break
-                else:
-                    current = current.right
 
 
     in_order(root)
 
     find_number = int(input())
 
-    c = root
-
-    while True:
-        if c is None:
-            print(f"cannot find {find_number}")
-            break
-        elif c.data is find_number:
-            print(f"find {find_number}!")
-            break
-        elif c.data > find_number:
-            c = c.left
-            continue
-        elif c.data < find_number:
-            c = c.right
-            continue
+    node.search(root, find_number)
