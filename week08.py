45c2234b19a2ca1aa8cb57c920e3c4087385275f
inhee <devforplayer@gmail.com>
2025-04-28 15:25:00 +0900

    v8.2 Binary search tree

--- week08.py
+++ week08.py
@@ -1,11 +1,9 @@
-def post_order(model):
-    if model is None:
-        return
 
-    post_order(model.left)
-    post_order(model.right)
-
-    print(model.data, end='->')
+class TreeNode:
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.data = None
 
 def in_order(model):
     if model is None:
@@ -15,46 +13,51 @@ def in_order(model):
     print(model.data, end='->')
     in_order(model.right)
 
-def pre_order(model):
-    if model is None:
-        return
+if __name__ == '__main__':
+    numbers = [10, 15, 8, 4, 9]
+    root = None
 
-    print(model.data, end='->')
+    node = TreeNode()
+    node.data = numbers[0]
+    root = node
 
-    pre_order(model.left)
-    pre_order(model.right)
+    for i in numbers[1:]:
+        n = TreeNode()
+        n.data = i
 
-class TreeNode:
-    def __init__(self):
-        self.left = None
-        self.right = None
-        self.data = None
-node1 = TreeNode()
-node1.data = 'Nayutan'
+        current = root
 
-node2 = TreeNode()
-node2.data = 'Deco*27'
-node1.left = node2
+        while True:
+            if current.data > n.data:
+                if current.left is None:
+                    current.left = n
+                    break
+                else:
+                    current = current.left
+            else:
+                if current.right is None:
+                    current.right = n
+                    break
+                else:
+                    current = current.right
 
-node3 = TreeNode()
-node3.data = 'Kasamura Tato'
-node1.right = node3
 
-node4 = TreeNode()
-node4.data = 'MIMI'
-node2.left = node4
+    in_order(root)
 
-node5 = TreeNode()
-node5.data = 'Threee'
-node2.right = node5
+    find_number = int(input())
 
-node6 = TreeNode()
-node6.data = 'PinocchioP'
-node3.left = node6
+    c = root
 
-post_order(node1)
-print()
-in_order(node1)
-print()
-pre_order(node1)
-print()
+    while True:
+        if c is None:
+            print(f"cannot find {find_number}")
+            break
+        elif c.data is find_number:
+            print(f"find {find_number}!")
+            break
+        elif c.data > find_number:
+            c = c.left
+            continue
+        elif c.data < find_number:
+            c = c.right
+            continue
