c062a56531e1066885d0bc9f57197b7ba88b8ec7
inhee <devforplayer@gmail.com>
2025-05-19 14:32:14 +0900

    v10.11 graph depth first search (boolean)

--- week11.py
+++ week11.py
@@ -7,14 +7,14 @@ graph = [[0, 1, 1, 0, 0, 0, 0, 0],
          [0, 0, 0, 1, 0, 0, 0, 1],
          [0, 0, 0, 0, 0, 0, 1, 0]]
 
-visited_gfs = [0 for _ in range(len(graph))]
+visited_gfs = [False for _ in range(len(graph))]
 
 def gfs(graph, index, visited):
-    visited[index] = 1
+    visited[index] = True
     print(chr(ord('A') + index), end='')
 
     for j in range(len(graph)):
-        if graph[index][j] == 1 and not visited[j]:
+        if graph[index][j] == 1 and not visiited[j]:
             gfs(graph, j, visited)
 
-gfs(graph, 4, visited_gfs)
+gfs(graph, 6, visited_gfs)
