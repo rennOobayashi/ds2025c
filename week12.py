342d952a867ad4e2350250e7423eff2f2bc535bf
inhee <devforplayer@gmail.com>
2025-05-26 14:11:44 +0900

    v11.1 breathe first search using deque

--- week12.py
+++ week12.py
@@ -1,9 +1,44 @@
 from collections import deque
 
-d = deque([17, 1, 2, 6, 4, 5])
-d.append(7)
-d.append(-9)
-d.appendleft(100)
+graph = [[0, 1, 1, 0, 0, 0, 0, 0],
+         [1, 0, 0, 1, 0, 0, 0, 0],
+         [1, 0, 0, 1, 0, 0, 0, 0],
+         [0, 1, 1, 0, 1, 1, 1, 0],
+         [0, 0, 0, 1, 0, 1, 0, 0],
+         [0, 0, 0, 1, 1, 0, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0, 1],
+         [0, 0, 0, 0, 0, 0, 1, 0]]
 
-for i in range(len(d)):
-    print(d.popleft())
+visited_dfs = [False for _ in range(len(graph))]
+visited_bfs = [False for _ in range(len(graph))]
+
+com_ls = []
+
+#depth first search
+def dfs(graph, index, visited):
+    visited[index] = True
+    print(chr(ord('A') + index), end=' ')
+
+    for j in range(len(graph)):
+        if graph[index][j] == 1 and not visited[j]:
+            dfs(graph, j, visited)
+
+#breadth first search
+def bfs(graph, index, visited):
+    q = deque([index])
+    visited[index] = True
+
+    while q:
+        i = q.popleft()
+        print(chr(ord('A') + i), end=' ')
+
+        for j in range(len(graph)):
+            if graph[i][j] == 1 and not visited[j]:
+                q.append(j)
+                visited[j] = True
+
+
+
+dfs(graph, 6, visited_dfs)
+print()
+bfs(graph, 4, visited_bfs)
