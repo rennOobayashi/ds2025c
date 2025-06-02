16990b0a9f1d5858cd78a776cf0c4fe274530cd6
inhee <devforplayer@gmail.com>
2025-06-02 15:17:55 +0900

    v12.2 low cost graph

--- week13-14.py
+++ week13-14.py
@@ -1,37 +1,44 @@
+from collections import deque
+
 class Graph:
 	def __init__ (self, size):
 		self.graph = [[0 for _ in range(size)] for _ in range(size)]
 
+class Disjoint:
+	def __init__(self, n):
+		self.parent = [i for i in range(n)]
+
+	def find(self, node):
+		if self.parent[node] != node:
+			self.parent[node] = self.find(self.parent[node])
+
+		return self.parent[node]
+
+	def marge(self, x, y):
+		x_root = self.find(x)
+		y_root = self.find(y)
+
+		if x_root != y_root:
+			self.parent[y_root] = x_root
+			return True
+
+		return False
+
+
 def print_graph(g) :
 	print(' ', end = ' ')
 	for v in range(len(g.graph)) :
-		print(name_ary[v], end =' ')
+		print(citys[v], end =' ')
 	print()
 	for row in range(len(g.graph)) :
-		print(name_ary[row], end =' ')
+		print(citys[row], end =' ')
 		for col in range(len(g.graph)) :
 			print(f"{g.graph[row][col]:2d}", end=' ')
 		print()
 	print()
 
-def dfs(graph, current, visited):
-	visited.append(current)
-
-	for vertex in range(graph_size):
-		if graph.graph[current][vertex] > 0 and vertex not in visited:
-			dfs(graph, vertex, visited)
-
-def bfs(graph, crurrent, visited):
-	pass
-
-def find_vertex(graph, find_vtx):
-	visited_arr = []
-	dfs(graph, 0, visited_arr)
-
-	return find_vtx in visited_arr
-
 g1 = None
-name_ary = ['인천', '서울', '강릉', '대전', '광주', '부산']
+citys = ['인천', '서울', '강릉', '대전', '광주', '부산']
 incheon, seoul, gangneung, daejeon, gwangju, busan = 0, 1, 2, 3, 4, 5
 
 
@@ -47,48 +54,34 @@ g1.graph[busan][daejeon] = 30; g1.graph[busan][gwangju] = 28
 print('도시 간 도로 건설을 위한 전체 연결도')
 print_graph(g1)
 
-edge_ary = []  # 결과적으로 2d list
+edges = []  # 결과적으로 2d list
 for i in range(graph_size) :
 	for k in range(graph_size) :
 		if g1.graph[i][k] != 0 :
-			edge_ary.append([g1.graph[i][k], i, k])
-print(edge_ary)
-
-edge_ary.sort(reverse=True)
-print(edge_ary)
-
-new_ary = list()
-for i in range(1, len(edge_ary), 2):
-	new_ary.append(edge_ary[i])
-print(new_ary)
-
-index = 0
-while len(new_ary) > graph_size - 1:	# 간선의 개수가 '정점 개수-1'일 때까지 반복
-	start = new_ary[index][1]
-	end = new_ary[index][2]
-	save_cost = new_ary[index][0]
-
-	g1.graph[start][end] = 0
-	g1.graph[end][start] = 0
-
-	start_reachable = find_vertex(g1, start)
-	end_reachable = find_vertex(g1, end)
-
-	if start_reachable and end_reachable :
-		del new_ary[index]
-	else:
-		g1.graph[start][end] = save_cost
-		g1.graph[end][start] = save_cost
-		index = index + 1
+			edges.append([g1.graph[i][k], i, k])
+print(edges)
 
 print('최소 비용의 도로 연결도')
 print_graph(g1)
 
-total_cost = 0
-for i in range(graph_size):
-	for k in range(graph_size):
-		if g1.graph[i][k] != 0:
-			total_cost = total_cost + g1.graph[i][k]
+edges.sort()
+print(edges)
 
-total_cost = total_cost // 2
-print(f"최소 비용의 도로 건설 비용 :  {total_cost}")
+ds = Disjoint(graph_size)
+mst_edges = []
+mst_cost = 0
+
+for cost, s, e in edges:
+	if ds.marge(s, e):
+		mst_edges.append((cost, s, e))
+		mst_cost =  mst_cost + cost
+
+mst_graph = Graph(graph_size)
+
+for w, s, e in mst_edges:
+	mst_graph.graph[s][e] = w
+	mst_graph.graph[e][s] = w
+
+print_graph(mst_graph)
+
+print(f"최소 비용의 도로 건설 비용 :  {mst_cost}")
