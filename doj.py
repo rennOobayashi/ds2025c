c87e61a32cd11342e3d8439292ceaa95965c371a
inhee <devforplayer@gmail.com>
2025-06-09 15:04:37 +0900

    v13.01 doj stack 10828

--- doj.py
+++ doj.py
@@ -5,7 +5,7 @@ cnt = int(input())
 for _ in range(cnt):
     s = input().split()
 
-    if "push" in s:
+    if "push" in s and len(s) > 1:
         ls.append(s[-1])
     elif "pop" in s:
         if len(ls) == 0:
@@ -15,6 +15,9 @@ for _ in range(cnt):
         print(ls[-1])
         ls.pop()
     elif "top" in s:
+        if len(ls) == 0:
+            print(-1)
+            continue
         print(ls[-1])
     elif "size" in s:
         print(len(ls))
